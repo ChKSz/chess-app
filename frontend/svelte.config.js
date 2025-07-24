b90043bc16701729b17324a77c227b29dfdedbdd
ChKSz <moriatychk@163.com>
2025-07-24 19:07:36 +0800

    fix(frontend): Remove svelte-preprocess for TypeScript

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,16 +1,10 @@
 import adapter from '@sveltejs/adapter-cloudflare';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		postcss: true,
-	}),
-
 	kit: {
 		adapter: adapter(),
 	},
-	// Move vite config here, outside of kit
 	vite: {
 		css: {
 			postcss: './postcss.config.js',
