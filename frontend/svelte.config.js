5fcdd2b4f681bb87d89b00618a0f8d401cec3334
ChKSz <moriatychk@163.com>
2025-07-24 18:39:35 +0800

    fix(frontend): Configure PostCSS in svelte.config.js for Tailwind

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,19 @@
 import adapter from '@sveltejs/adapter-cloudflare';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: preprocess({
+		postcss: true,
+	}),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		vite: {
+			css: {
+				postcss: './postcss.config.js',
+			},
+		},
 	}
 };
 
